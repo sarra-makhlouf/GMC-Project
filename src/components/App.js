6b58456b7e3910e1b80787f024963df7490d42bb
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2019-09-08 02:47:08 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TicketsList from "./TicketsList";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hello</h2>
+      <TicketsList title="testing title" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from "redux";
+
+// temporary reducer
+const reducer = () => {};
+
+const store = createStore(reducer);
+
+export default store;
